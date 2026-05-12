e491bf2c4d2e400ae66af3029252f8df996dd2ed
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2026-05-12 10:58:49 +0100

    auth swagger doc added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,74 @@ const router = express.Router();
 const { login, getMe } = require("../controllers/authController");
 const { protect } = require("../middleware/auth");
 
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Admin login
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - password
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: admin
+ *               password:
+ *                 type: string
+ *                 example: changeme123
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 token:
+ *                   type: string
+ *                 admin:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                     username:
+ *                       type: string
+ *       401:
+ *         description: Invalid credentials
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 router.post("/login", login);
+
+/**
+ * @swagger
+ * /auth/me:
+ *   get:
+ *     summary: Get current admin
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Current admin details
+ *       401:
+ *         description: Not authorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 router.get("/me", protect, getMe);
 
 module.exports = router;
